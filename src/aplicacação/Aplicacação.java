6395ee662cb286afa43970b6990d9c54b97831d5
31608914 <31608914@087916-P33S103.macklabs.br>
2017-11-01 11:21:53 -0200

    Inclusão de Variavel x

--- src/aplicacação/Aplicacação.java
+++ src/aplicacação/Aplicacação.java
@@ -15,7 +15,9 @@ public class Aplicacação {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int x =0;
+        
     }
     
 }
